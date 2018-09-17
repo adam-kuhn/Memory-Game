0425ee2b0d639db5006665b9cd0b0720325bb439
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-17 14:13:50 +0300

    rmv redux in code

--- client/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const SHOW_GAME = 'SHOW_GAME'
-
-export const startGame = () => {
-  return {
-    type: SHOW_GAME
-  }
-}
--- client/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-
-import display from './display'
-
-export default combineReducers({
-  display
-})
