d2b0b2654b48f51e56394da2cb28db82a52fe2fc
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-18 13:07:03 +0300

    make 10 cards and 5 pairs for game

--- client/lib/gameboard.js
+++ client/lib/gameboard.js
@@ -21,4 +21,41 @@ export default [{
   value: 2,
   visible: false,
   matched: false
-}]
+},
+{
+  id: 5,
+  value: 3,
+  visible: false,
+  matched: false
+},
+{
+  id: 6,
+  value: 3,
+  visible: false,
+  matched: false
+},
+{
+  id: 7,
+  value: 4,
+  visible: false,
+  matched: false
+},
+{
+  id: 8,
+  value: 4,
+  visible: false,
+  matched: false
+},
+{
+  id: 9,
+  value: 5,
+  visible: false,
+  matched: false
+},
+{
+  id: 10,
+  value: 5,
+  visible: false,
+  matched: false
+}
+]
