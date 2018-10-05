938f86907870e55400519982d48907ef7e8ec28e
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-05 14:39:52 +0300

    add basic counter component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Game from './Game'
+import Counter from './Counter'
 
 const App = () => {
   return (
     <div>
       <h2>Memory Game</h2>
+      <Counter />
       <Game />
     </div>
   )
---
+++ client/components/Counter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <p>Score: 0</p>
+  )
+}
+
+export default Counter
