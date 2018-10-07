76d28a547ca0469c2b8ae2681a16a26f44d67475
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-07 09:07:58 +0300

    add ranking to score and alert

--- client/lib/displayWin.js
+++ client/lib/displayWin.js
@@ -1,9 +1,11 @@
 import swal from 'sweetalert2'
 
 const displayWin = (score) => {
+  let ranking = score < 14 ? 'Lucky' : score < 20 ? 'Smart' : score < 30 ? 'Dog' : 'Bug'
+
   swal({
     type: 'success',
-    title: 'YOU WIN!',
+    title: `YOU WIN! - Ranking: ${ranking}`,
     text: `Well done! It took you ${score} clicks to match them all.`
   })
 }
